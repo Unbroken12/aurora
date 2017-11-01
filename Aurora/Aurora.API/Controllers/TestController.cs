52e343dbe2077f81d73032acaf1e6d094db43c5a
Daemon 025 <daemon025@gmail.com>
2017-11-01 14:36:20 +0200

    Initial project commit

---
+++ Aurora/Aurora.API/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aurora.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    public class TestController : Controller
+    {
+        public ActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
