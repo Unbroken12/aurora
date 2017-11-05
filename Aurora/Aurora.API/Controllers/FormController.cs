1c81bf6b491cd77b88bfa25021168124c21945bd
Daemon 025 <daemon025@gmail.com>
2017-11-05 15:32:15 +0200

    project structure

---
+++ Aurora/Aurora.API/Controllers/FormController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace Aurora.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Form")]
+    public class FormController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public FormController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET: api/Form
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Form/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Form
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Form/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Aurora/Aurora.API/Startup.cs
+++ Aurora/Aurora.API/Startup.cs
@@ -15,7 +15,9 @@ namespace Aurora.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddJsonOptions(j => {
+                
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +36,7 @@ namespace Aurora.API
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                
             });
         }
     }
